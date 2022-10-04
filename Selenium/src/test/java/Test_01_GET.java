f5e5ab95a04dbc335ee36c0baeaccd18f3f79ab7
Quyen Tran <quyen.tran@team.enouvo.com>
2022-10-04 16:59:13 +0700

    Add post automation

--- Selenium/src/test/java/Test_01_GET.java
+++ Selenium/src/test/java/Test_01_GET.java
@@ -1,12 +1,13 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
-
+import io.restassured.specification.RequestSpecification;
 public class Test_01_GET {
     @Test
     void test_01() {
@@ -29,5 +30,36 @@ public class Test_01_GET {
                 .statusCode(200)
                 .body("data.id[0]", equalTo(7));
     }
+
+    @Test
+    void test_03() {
+        RequestSpecification request = RestAssured.given();
+        // JSONObject is a class that represents a Simple JSON.
+
+        // We can add Key - Value pairs using the put method
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("email", "eve.holt@reqres.in");
+        requestParams.put("password", "pistol");
+
+        // Add a header stating the Request body is a JSON
+        request.header("Content-Type", "application/json");
+
+        // Add the Json to the body of the request
+        request.body(requestParams.toJSONString());
+
+        // Post the request and check the response
+        Response response = request.post("https://reqres.in/api/register");
+
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
+        Assert.assertEquals(statusCode, 200);
+        JSONObject expected = new JSONObject();
+        expected.put("id", 4);
+        expected.put("token", "QpwL5tke4Pnpja7X4");
+
+        String successResult = response.body().print();
+        Assert.assertEquals(successResult, expected.toJSONString());
+    }
+
 }
 // Document https://github.com/rest-assured/rest-assured/wiki/Usage#static-imports
